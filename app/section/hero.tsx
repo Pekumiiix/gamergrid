d36955514b1f0c26802b540ca5a88373aa5a16c5
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2024-12-15 21:06:54 +0100

    Update hero.tsx

--- app/section/hero.tsx
+++ app/section/hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function HeroSection() {
   return (
-    <section className="container flex flex-col items-center lg:items-start gap-5">
+    <section className="container flex flex-col items-center lg:items-start gap-5 px-5">
       <Image src={`/logo-2.png`} alt="Logo" width={600} height={53} />
       <p className="text-base md:text-3xl text-center lg:text-left font-medium text-orange-200 max-w-[75%]">
         Smart Contract Interactive UI.
